35b0568c72131922170962123e92ff32b3739197
Mike Abelar <mikeabelar1999@gmail.com>
2020-03-30 13:32:39 -0400

    Update to 1.02

--- Formula/hackstart.rb
+++ Formula/hackstart.rb
@@ -1,15 +1,12 @@
 class Hackstart < Formula
   desc "Set up a Hack project with one line"
   homepage "https://github.com/mike2151/hackstart"
-  url "https://github.com/mike2151/hackstart/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "a5b3e7f4c4afc8f249b45a95e46702de221220ce290a48a110bc02ac0641ee46"
+  url "https://github.com/mike2151/hackstart/archive/1.02.tar.gz"
+  version "1.0.2"
+  sha256 "397fb9cc5ccbea31ffc1f61ebb1e4e22674c350a19362eb2ba31b837de003cc1"
 
   def install
     bin.install "bin/hackstart"
   end
 
 end
-
-
-
